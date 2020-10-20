afb11a601ecf2aafc9e96343519abb95539421bc
wisnubayuaji <wisnubayu34@gmail.com>
2020-10-20 19:42:18 +0700

    Add files via upload

---
+++ if2-10116430-Latihan8-Pecahan/src/App.java
@@ -0,0 +1,16 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+}
